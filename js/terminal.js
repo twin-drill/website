50f27da356cda5b3e2a885727f4c055a75e3aed1
tenqo <3axesofficial@gmail.com>
2021-12-19 03:25:37 -0600

    .

--- js/terminal.js
+++ js/terminal.js
@@ -2,19 +2,27 @@
 This should parse entered text from the terminal and redirect/output resources
 possibly catch 404s, idk
 
-format of a baseline:
-o2i6@localhost:~$<sp>
+add a 'session closed' at the end of previously cached linefeed possibly
  */
 
 const LINE = "o2i6@localhost:~$ ";
+const cookies = document.cookie;
 
-function enterToSubmit(evt) {
-    evt.preventDefault();
-    if (evt.keyCode == 13) { readInput(); }
+window.onload = function() {
+    updateHistory();
+    document.getElementById("command").addEventListener("keypress",
+        function(event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            document.getElementById("command").disabled = true;
+            readInput();
+        }
+    }, false);
 }
 
-function load() {
-    document.getElementByID("command").addEventListener("keydown", enterToSubmit(Event));
+function updateHistory() {
+    // TODO figure out how to actually get newlines to work
+    document.getElementById("history").textContent += "\r\nSession terminated.\r\n" + LINE;
 }
 
 function readInput() {
@@ -26,5 +34,33 @@ function readInput() {
     // for now
     console.log("parsed");
     console.log(command);
-
+    document.getElementById("command").disabled = false;
+    document.getElementById("command").focus();
 }
+
+
+//argparse example
+// - prefix for arguments with parameters
+// -- prefix for boolean-esque options
+function argparse(args) {
+    let option1 = false;
+    let option2 = false;
+    for (let i = 0; i < args.length; ++i) {
+        if (args[i].startsWith("--")) {
+            switch (args[i]) {
+                case "option1" : option1 = true; break;
+            }
+        }
+        if (args[i].startsWith("-")) {
+            // setCommand
+            let newargs = [];
+            while (!args[i].startsWith("-")) {
+                // newargs append arg
+            }
+        }
+    }
+}
+
+// regex split of a multiline string
+// var result = Regex.Split(text, "\r\n|\r|\n");
+// to be used in update() for refreshing text displayed in window (limit to n lines at a time)
